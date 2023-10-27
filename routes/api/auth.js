6f9c8538efa44ae786744a5527fa58c81200a33c
raghuveertech <raghuveerp25@gmail.com>
2023-10-27 18:33:47 +0530

    user registration, login API - complete, with JWT authentication

---
+++ routes/api/auth.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("./../../models/User");
+const auth = require("../../middleware/auth");
+
+// @route    POST api/auth
+// @desc     Authenticate User & get token
+// @access   Public
+
+router.post(
+  "/",
+  [
+    check("email", "Please enter a valid email address").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid Credentials" }],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid Credentials" }],
+        });
+      }
+
+      // return Json Web Token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+      //
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+  res.send("Auth Route");
+});
+
+module.exports = router;
